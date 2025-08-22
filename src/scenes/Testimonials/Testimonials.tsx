be260d30aaa69e05ee1f8bc44e3ee5732cfcf2bf
gugdun <gugdunoff@gmail.com>
2025-08-22 18:29:53 +0300

    add: Testimonials card layout

--- src/scenes/Testimonials/Testimonials.tsx
+++ src/scenes/Testimonials/Testimonials.tsx
@@ -15,6 +15,20 @@ export function Testimonials({ register }: SceneProps) {
   return (
     <section ref={container} class="testimonials">
       <h1 class="title">What our passengers say</h1>
+      <div class="cards">
+        <div id="quote1" class="card">
+            <p class="quote">“Lunair made flying a pleasure. I never thought the road could be so easy!”</p>
+            <p class="author">— Leah Schmid</p>
+        </div>
+        <div id="quote2" class="card">
+            <p class="quote">“Excellent service and amazing comfort. Will fly again!”</p>
+            <p class="author">— Riley Ayers</p>
+        </div>
+        <div id="quote3" class="card">
+            <p class="quote">“I felt like I was traveling to the future. Thanks Lunair!”</p>
+            <p class="author">— Ella Wilding</p>
+        </div>
+      </div>
     </section>
   );
 }
--- src/scenes/Testimonials/testimonials.anim.ts
+++ src/scenes/Testimonials/testimonials.anim.ts
@@ -7,9 +7,27 @@ export function createTestimonialsAnimation(container: HTMLElement) {
   const titleTl = gsap
     .timeline()
     .fromTo(title, { y: 200, opacity: 0 }, { y: 0, opacity: 1, duration: 1 })
-    .to(title, { y: -200, opacity: 0, duration: 1 });
+    .to(title, { y: 0, opacity: 1, duration: 3 });
 
-  tl.add(titleTl, 0);
+  const quote1 = container.querySelector("#quote1");
+  const quote1Tl = gsap
+    .timeline()
+    .fromTo(quote1, { y: 200, opacity: 0 }, { y: 200, opacity: 0, duration: 1 })
+    .to(quote1, { y: 0, opacity: 1, duration: 1 });
+
+  const quote2 = container.querySelector("#quote2");
+  const quote2Tl = gsap
+    .timeline()
+    .fromTo(quote2, { y: 200, opacity: 0 }, { y: 200, opacity: 0, duration: 2 })
+    .to(quote2, { y: 0, opacity: 1, duration: 1 });
+
+  const quote3 = container.querySelector("#quote3");
+  const quote3Tl = gsap
+    .timeline()
+    .fromTo(quote3, { y: 200, opacity: 0 }, { y: 200, opacity: 0, duration: 3 })
+    .to(quote3, { y: 0, opacity: 1, duration: 1 });
+
+  tl.add(titleTl, 0).add(quote1Tl, 0).add(quote2Tl, 0).add(quote3Tl, 0);
 
   return tl;
 }
